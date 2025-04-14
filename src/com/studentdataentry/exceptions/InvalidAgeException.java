834986212c9530572628863d2507f5036fc1bedc
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-14 22:13:57 +0530

    Initialised InvalidAgeException

---
+++ src/com/studentdataentry/exceptions/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package com.studentdataentry.exceptions;
+
+public class InvalidAgeException extends RuntimeException {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
